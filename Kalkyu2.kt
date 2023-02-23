be1c5f547e756cdd3ea7add5e08fd691690a4af7
FoxJayOur <dimacjoshryan@gmail.com>
2023-02-23 11:26:27 +0800

    2 commit

--- Kalkyu2.kt
+++ Kalkyu2.kt
@@ -10,6 +10,31 @@ fun pow(n: Double, m: Double): Any {
         else -> 1
     }
 }
+
+fun factorial(n: Double, m: Double): Any {
+    var result = n
+    for (i in n downTo 1) {
+        result = result * (i - 1)
+    }
+}
+// add 0 in 2nd number for validation
+fun perm (n: Double, r: Double): Any {
+
+}
+
+fun percentage (n: Double, m: Double): Any {
+    var result = n/m
+    var percent = result * 100
+
+    println("$n over $m percentage is $percent %")
+}
+
+fun percentage_t (n: Double): Any {
+    n = n / 100
+    println("Percentage translation is $n")
+}
+
+
 fun main() {
     println("Enter your arithmetic operation: (Please use space for readability)")
 
@@ -24,7 +49,6 @@ fun main() {
             val operator: String = values[1]
             val num1 = values[0].toDoubleOrNull() ?: throw IllegalArgumentException("INVALID INPUT: ${values[0]}")
             val num2 = values[2].toDoubleOrNull() ?: throw IllegalArgumentException("INVALID INPUT: ${values[2]}")
-
             val answer = when (operator) {
                 "+" -> num1 + num2
                 "-" -> num1 - num2
@@ -32,6 +56,7 @@ fun main() {
                 "/" -> num1/num2
                 "%" -> num1 % num2
                 "^" -> pow(num1, num2)
+                "!" -> factorial (num1, num2)
 
                 else -> throw IllegalArgumentException("INVALID OPERATOR: $operator")
             }
