74f3cd5c884a700d83e8195c387e6c11f017e303
FoxJayOur <dimacjoshryan@gmail.com>
2023-02-23 22:27:42 +0800

    edit

--- Kalkyu.kt
+++ Kalkyu.kt
@@ -10,6 +10,20 @@ fun pow(n: Double, m: Double): Any {
         else -> 1
     }
 }
+
+fun factorial(n: Int): Any {
+  var result = n
+  for(i in n downTo 1) {
+    if (i == 1) {
+      break
+    }
+    else {
+      result = result * (i - 1)
+    }
+  }
+  println (result)
+  return result
+}
 fun main() {
     println("Enter your arithmetic operation: (Please use space for readability)")
 
@@ -17,8 +31,17 @@ fun main() {
 
     while (input != null && input.isNotBlank()) {
         val values: List<String> = input.split(' ')
+        
+        if (values.size == 2) {
+            val operator: String = values[1]
+            val num3 = values[0].toIntOrNull() ?: throw IllegalArgumentException("INVALID INPUT: ${values[2]}")
+            val answer = when (operator) {
+                "!" -> factorial(num3)
 
-        if (values.size < 3) {
+                else -> throw IllegalArgumentException("INVALID OPERATOR: $operator")
+            }
+        }
+        else if (values.size < 2) {
             println("Invalid input. Expected: value + value. Received: $input")
         } else {
             val operator: String = values[1]
